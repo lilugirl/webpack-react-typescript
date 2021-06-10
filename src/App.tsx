5d67aa6eac1a54e5cfbe7621aa97e5a928379785
liu yi <lilugirl2005@126.com>
2021-06-10 16:57:28 +0800

    引入第三方node库

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Button from './Button';
+import {Sum} from 'liuyi-node-project';
 class App extends React.Component{
     render(){
         return(<div>
             <p>hi world</p>
+            {Sum(10,32)}
            <Button></Button>
         </div>)
     }
